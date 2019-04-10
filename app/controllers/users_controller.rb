cbccff044efdaacb00af3ecf65b49e279df8a64e
lhsharing <daley.luis@gmail.com>
2019-04-10 15:42:38 -0400

    added signup and login erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 #class AlumniController < Sinatra::Base
-class UsersController < ActiveRecord::Base
+class UsersController < ApplicationController
 has_secure_password
 has_many :meetups
 end
+
+
+#routes needed for login
+#purpose = render the login page (form)
+get '/login' do
+    erb :login
+end
+
+#routes needed for signup
+get '/signup' do
+    erb :signup
+end
+
+
 
 
     # configure do

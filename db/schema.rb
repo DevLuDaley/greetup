979654ded112515ec433bbf55ae904043cd180f2
lhsharing <daley.luis@gmail.com>
2019-04-10 13:08:25 -0400

    reseeded db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410134751) do
+ActiveRecord::Schema.define(version: 20190410155644) do
+
+  create_table "coffees", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "giants", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,20 @@
 require_relative('/Users/LHD/Development/code/Learn_Labs/ONLINE-web-ft-021119/Week8_Sinatra_Project_Week/greetup/app/models/greetup.rb')
 
-Greetup.create(name: "Happy Hour", location: "Bronx, NY", user_id: 1)
-Greetup.create(name: "Sadie Hawkins Dance", location: "Queens, NY", user_id: 2)
-Greetup.create(name: "Lunch & Learn", location: "Queens, NY", user_id: 1)
+#create users
+rakim = User.create(user_name: "nykid", email: "coolemail@aol.com", city: "New York", state: "New York", zipcode: "somewhere", password:"welcome")
+andre3000 = create(user_name: "nhkid", email: "goodemail@aol.com", city: "Concord", state: "New Hampshire", zipcode: "somewhere", password:"welcome")
+capri = create(user_name: "bkid", email: "okayemail@aol.com", city: "Baltimore", state: "Maryland", zipcode: "somewhere", password:"welcome")
 
-User.create(user_name: "nykid", email: "coolemail@aol.com", city: "New York", state: "New York", zipcode: "somewhere", password:"welcome")
-User.create(user_name: "Nhkid", email: "goodemail@aol.com", city: "Concord", state: "New Hampshire", zipcode: "somewhere", password:"welcome")
-User.create(user_name: "Bkid", email: "okayemail@aol.com", city: "Baltimore", state: "Maryland", zipcode: "somewhere", password:"welcome")
+#create greetups
+happy = Greetup.create(name: "Happy Hour", location: "Bronx, NY", user_id: rakim.id)
 
-Giant.create(name: "Hodor", location: "Bronx, NY", user_id: 1)
-Giant.create(name: "One One", location: "Queens, NY", user_id: 2)
-Giant.create(name: "The Mountain", location: "Queens, NY", user_id: 1)
+#use AR to pre-associate data
+capri.greetups.create(name: "DJ Dance Party", location: "Queens, NY",)
+lunch = Greetup.create(name: "Lunch & Learn", location: "Queens, NY", user_id: 4)
+
+
+#create giants
+hodor = Giant.create(name: "Hodor", location: "Bronx, NY", user_id: 1)
+one_one = Giant.create(name: "One One", location: "Brooklyn, NY", user_id: 2)
+the_mountain = Giant.create(name: "The Mountain", location: "Queens, NY", user_id: 1)
+

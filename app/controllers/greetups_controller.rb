fa9dd4299b63cfadec438db5937f4baa00824883
lhsharing <daley.luis@gmail.com>
2019-04-12 19:16:53 -0400

    added /love to test flash

--- app/controllers/greetups_controller.rb
+++ app/controllers/greetups_controller.rb
@@ -17,15 +17,20 @@ class GreetupsController < ApplicationController
     erb :"greetups/index"
   end
 
+  get '/greetups/love' do
+    flash[:hello_world] = "hello world!"
+    erb :"greetups/love"
+  end
+
 #2
 # GREETUPS CREATE/NEW PAGE #
   get '/greetups/new' do
     #redirect_if_not_logged_in
-  if !logged_in?
-    redirect "/"
+  #if !logged_in?
+  #  redirect "/"
     erb :'greetups/new'
-  end
   #end
+  end
 
 post '/greetups' do
 #raise params.inspect
@@ -36,13 +41,13 @@ if !logged_in?
 redirect "/"
 end
 
-end
+#end
 #binding.pry
 if params[:name] != "" && params[:location] != ""
   #create new entry
   @greetup = Greetup.create(name: params[:Name],location: params[:Location], user_id: current_user.id)
   #, title: params[:title], mood: params[:mood]))
-   flash[:message] = "Greetup successfully created." if @greetup.id
+   flash[:message] = "Greetup successfully created." #if @greetup.id
   redirect "greetups/#{@greetup.id}"
 else
   flash[:errors] = "Something went wrong - you must provide information for your event."
@@ -55,7 +60,8 @@ end
 # GREETUPS SHOW PAGE #
 #show route for greetups page
 get '/greetups/:id' do
-#@greetup = Greetup.find(params[:id])
+@greetup = Greetup.find(params[:id])
+#@greetup = Greetup.find_by(id: params[:id])
 set_greetup
 erb :'/greetups/show'
 end
@@ -65,31 +71,31 @@ end
 #render edit form
  get '/greetups/:id/edit' do
     #redirect_if_not_logged_in
-    set_greetup
+    #binding.pry
+    @greetup = Greetup.find_by(params[:id])
+    #set_greetup
 if authorized_to_edit?(@greetup)
-    erb :"/greetup/edit"
+    erb :"/greetups/edit"
 else
     redirect "/users/#{current_user.id}"
 end
   end
 
 #update greetup
- patch '/journal_entries/:id' do
+ patch '/greetups/:id' do
     #redirect_if_not_logged_in
-    # 1. find the journal entry
-    set_greetup
+    # 1. find the greetup
+    #set_greetup
+    @greetup = Greetup.find(params[:id])
     if @greetup.user == current_user && params[:name] != ""
-    # 2. modify (update) the journal entry
+    # 2. modify (update) the greetup
       @greetup.update(name: params[:name])
       # 3. redirect to show page
-      redirect "/journal_entries/#{@greetup.id}"
+      redirect "/greetups/#{@greetup.id}"
     else
       redirect "users/#{current_user.id}"
     end
   end
-
-
-
 
 delete '/greetups/:id' do
   set_greetup
@@ -105,17 +111,5 @@ end
   def set_greetup
     @greetup = Greetup.find(params[:id])
   end
-
-
-    # configure do
-# set :public_folder, 'public'
-# set :views, 'app/views'
-# enable :sessions
-# set :session_secret, "password_security"
-# end
-# get '/' do
-# "Hello, World!"
-# end
-# end
 
 end

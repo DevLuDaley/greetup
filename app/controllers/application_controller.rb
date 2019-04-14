0989355317244c01cefd50f437750045281bf65e
lhsharing <daley.luis@gmail.com>
2019-04-14 13:34:41 -0400

    +commented out code from AliciaS to hunt id bug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,9 @@ end
     
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
+      #code from Alicia Santiago -> 
+          ##session[:user_id] && User.find_by(id: session[:user_id])
+
       #use memoization, it will reduce the number of database calls
       #will return user if there is one
       #instance variable has scope outside of method, extends to class.

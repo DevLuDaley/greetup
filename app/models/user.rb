9be630019632162652489f4eccb9bb91ad1835f2
lhsharing <daley.luis@gmail.com>
2019-04-13 23:41:39 -0400

    update_ syntax mistakes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 has_secure_password
 #validates_uniqueness_of :username
   validates :name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :user_name, presence: true
+  validates :user_name, uniqueness: true
 
 has_many :greetups
 end
